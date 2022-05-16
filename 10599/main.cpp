8391c7f8f18b41a1b79438dec475ac7f202904f8
Emilia <emiliaistruelove@gmail.com>
2022-05-17 02:42:20 +0800

    remove extra file

--- 10599/main.cpp
+++ 10599/main.cpp
@@ -12,10 +12,10 @@ typedef pair<int, int> pii;
 
 constexpr int MAXN = 100;
 
-int cnts[MAXN + 5][MAXN + 5];
-int ways[MAXN + 5][MAXN + 5];
-pii from[MAXN + 5][MAXN + 5];
-bool vis[MAXN + 5][MAXN + 5];
+int cnts[MAXN * MAXN + 10];
+int ways[MAXN * MAXN + 10];
+int from[MAXN * MAXN + 10];
+int pts[MAXN * MAXN + 10];
 bool g[MAXN + 5][MAXN + 5];
 
 int main()
@@ -24,84 +24,57 @@ int main()
 	while(~scanf("%d %d", &n, &m) && !(n == -1 && m == -1))
 	{
 		memset(cnts, 0, sizeof(cnts));
-		memset(g, 0, sizeof(g));
+		memset(g, false, sizeof(g));
 		memset(ways, 0, sizeof(ways));
-		memset(vis, 0, sizeof(vis));
-		queue<pii> qu;
+		memset(from, -1, sizeof(from));
 		
-		int fix = -2, x, y;
+		int x, y, L = 0;
+		
 		while(scanf("%d %d", &x, &y) && !(x == 0 && y == 0))
-		{
-			cnts[x][y] = 1;
 			g[x][y] = true;
 
-			if (x == 1 && y == 1)
-				++fix;
-			if (x == n && y == m)
-				++fix;
-		}
+		for (int i = 1; i <= n; ++i)
+			for (int j = 1; j <= m; ++j)
+				if (g[i][j]) pts[L++] = (i - 1) * m + j;
+
 		
-		if (fix > 0) fix = 0;
-		qu.emplace(pii{1, 1});
-		ways[1][1] = cnts[1][1] = cnts[n][m] = 1;
-		vis[1][1] = true;
-		while(qu.size())
+		if (!g[n][m]) pts[L++] = n * m;
+
+		for (int i = 0; i < L; ++i)
 		{
-			pii now = qu.front(); qu.pop();
-			//printf("%d %d\n", now.first, now.second);
-			int upperm = m;
-			for (int i = now.first; i <= n; ++i)
-				for (int j = now.second; j < upperm; ++j)
+			ways[i] = cnts[i] = 1;
+			from[i] = -1;
+			for (int j = 0; j < i; ++j)
+				if ((pts[j] - 1) % m <= (pts[i] - 1) % m)
 				{
-					if (i == now.first && j == now.second) continue;
-					if (cnts[i][j])
+					if (cnts[i] < cnts[j] + 1)
 					{
-						//printf("%d %d\n", i, j);
-						upperm = min(upperm, j);
-						if (cnts[i][j] < cnts[now.first][now.second] + 1)
-						{
-							from[i][j] = pii{now.first, now.second};
-							cnts[i][j] = cnts[now.first][now.second] + 1;
-							ways[i][j] = ways[now.first][now.second];
-						}
-						else if (cnts[i][j] == cnts[now.first][now.second] + 1)
-						{
-							ways[i][j] += ways[now.first][now.second];
-						}
-						
-						if (!vis[i][j])
-						{
-							vis[i][j] = true;
-							qu.emplace(pii{i, j});
-						}
-						
-						//qu.emplace(pii{i, j});
-						break;
+						from[i] = j;
+						cnts[i] = cnts[j] + 1;
+						ways[i] = ways[j];
 					}
+					else if (cnts[i] == cnts[j] + 1)
+						ways[i] += ways[j];
 				}
 		}
 
-		printf("CASE#%d: %d %d", kase++, cnts[n][m] + fix, ways[n][m]);
+		if (!g[n][m]) --cnts[L - 1];
+		printf("CASE#%d: %d %d", kase++, cnts[L - 1], ways[L - 1]);
 		
-		pii p{n, m};
-		deque<pii> dq;
-		while(!(p.first == 1 && p.second == 1))
+		int p = L - 1;
+		deque<int> dq;
+		while(p != -1)
 		{
-			if (g[p.first][p.second])
-				dq.emplace_front(p);
-			p = from[p.first][p.second];
+			dq.emplace_front(pts[p]);
+			p = from[p];
 		}
-		if (g[1][1])
-			dq.emplace_front(p);
+		if (!g[n][m])
+			dq.pop_back();
 		
 		if (dq.size())
-		{
-			printf(" %d", (dq[0].first - 1) * m + dq[0].second);
-			for (auto it = dq.begin() + 1; it != dq.end(); ++it)
-			{
-				printf(" %d", ((*it).first - 1) * m + (*it).second);
-			}
-		}
+			for (auto it = dq.begin(); it != dq.end(); ++it)
+				printf(" %d", *it);
+
 		puts("");
 	}
 	return 0;
