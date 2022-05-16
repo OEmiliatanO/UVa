98431319e02e87fe0fdaa1b970f4a3c108292ec1
Emilia <emiliaistruelove@gmail.com>
2022-05-16 15:36:05 +0800

    update 10599 (remake unAC)

--- 10599/main.cpp
+++ 10599/main.cpp
@@ -11,92 +11,47 @@ typedef pair<int, int> pii;
 
 constexpr int MAXN = 100;
 
-int ways[MAXN + 1][MAXN + 1];
-int cnts[MAXN + 1][MAXN + 1];
-int from[MAXN + 1][MAXN + 1];
-bool inqu[MAXN + 1][MAXN + 1];
-vector<pii> xy, yx;
+int cnts[MAXN + 5][MAXN + 5];
+int ways[MAXN + 5][MAXN + 5];
 
 int main()
 {
-	cout << "1" << endl;
 	int n, m;
 	while(~scanf("%d %d", &n, &m) && !(n == -1 && m == -1))
 	{
-		memset(ways, 0, sizeof(ways));
 		memset(cnts, 0, sizeof(cnts));
-		memset(inqu, 0, sizeof(inqu));
-		xy.clear(); yx.clear();
+		memset(ways, 0, sizeof(ways));
 
 		queue<pii> qu;
-		int x, y;
-
+		int fix = -2;
 		while(scanf("%d %d", &x, &y) && !(x == 0 && y == 0))
 		{
-			++cnts[x][y];
-			xy.emplace_back(x, y);
-			yx.emplace_back(y, x);
+			cnts[x][y] = 1;
+			if (x == 1 && y == 1)
+				++fix;
+			else if (x == n && y == m)
+				++fix;
 		}
-		if (cnts[n][m] <= 0)
-		{
-			xy.emplace_back(n, m);
-			yx.emplace_back(m, n);
-		}
-
-		sort(xy.begin(), xy.end());
-		sort(yx.begin(), yx.end());
-
+		
+		if (fix > 0) fix = 0;
 		qu.emplace(1, 1);
-		ways[1][1] = 1;
+		cnts[1][1] = cnts[n][m] = 1;
 		while(qu.size())
 		{
-			pii t = qu.front(); qu.pop();
-			auto p = lower_bound(xy.begin(), xy.end(), t);
-			auto k = lower_bound(yx.begin(), yx.end(), pii{t.second, t.first});
-
-			if (p != xy.end())
-			{
-				int xx = (*p).first;
-				while(p->first == xx)
+			pii now = qu.front(); qu.pop();
+			int upperm = m;
+			for (int i = now.first; i <= n; ++i)
+				for (int j = now.second; j <= upperm; ++j)
 				{
-					ways[p->first][p->second] += ways[t.first][t.second];
-					if (cnts[t.first][t.second] + 1 > cnts[p->first][p->second])
+					if (cnts[i][j])
 					{
-						cnts[p->first][p->second] = cnts[t.first][t.second] + 1;
-						from[p->first][p->second] = t.first * n + t.second;
+						upperm = min(upperm, j);
+						cnts[i][j] = max(cnts[i][j], cnt[now.first][now.second] + 1);
+						ways[i][j] = 
+						break;
 					}
-					if (!inqu[p->first][p->second])
-					{
-						qu.emplace(*p);
-						inqu[p->first][p->second] = true;
-					}
-					++p;
 				}
-			}
-
-			if (k != yx.end())
-			{
-				int yy = k->first;
-				while(k->first == yy)
-				{
-					ways[k->second][k->first] += ways[t.first][t.second];
-					if (cnts[t.first][t.second] + 1 > cnts[k->second][k->first])
-					{
-						cnts[k->second][k->first] = cnts[t.first][t.second] + 1;
-						from[k->second][k->first] = t.first * n + t.second;
-					}
-					if (!inqu[k->second][k->first])
-					{
-						qu.emplace(*k);
-						inqu[k->second][k->first] = true;
-					}
-					++k;
-				}
-			}
 		}
-
-		printf("%d %d", cnts[n][m], ways[n][m]);
-		puts("");
 	}
 	return 0;
 }
