6e809f1a6ad44907645f43ef921fb328404104c5
OEmiliatanO <47825105+OEmiliatanO@users.noreply.github.com>
2022-05-16 16:43:30 +0800

    update 10599 (partial done)

--- 10599/main.cpp
+++ 10599/main.cpp
@@ -13,6 +13,7 @@ constexpr int MAXN = 100;
 
 int cnts[MAXN + 5][MAXN + 5];
 int ways[MAXN + 5][MAXN + 5];
+bool vis[MAXN + 5][MAXN + 5];
 
 int main()
 {
@@ -21,9 +22,9 @@ int main()
 	{
 		memset(cnts, 0, sizeof(cnts));
 		memset(ways, 0, sizeof(ways));
-
+		memset(vis, 0, sizeof(vis));
 		queue<pii> qu;
-		int fix = -2;
+		int fix = -2, x, y;
 		while(scanf("%d %d", &x, &y) && !(x == 0 && y == 0))
 		{
 			cnts[x][y] = 1;
@@ -34,11 +35,12 @@ int main()
 		}
 		
 		if (fix > 0) fix = 0;
-		qu.emplace(1, 1);
+		qu.emplace(pii{1, 1});
 		cnts[1][1] = cnts[n][m] = 1;
 		while(qu.size())
 		{
 			pii now = qu.front(); qu.pop();
+			vis[now.first][now.second] = true;
 			int upperm = m;
 			for (int i = now.first; i <= n; ++i)
 				for (int j = now.second; j <= upperm; ++j)
@@ -46,12 +48,18 @@ int main()
 					if (cnts[i][j])
 					{
 						upperm = min(upperm, j);
-						cnts[i][j] = max(cnts[i][j], cnt[now.first][now.second] + 1);
-						ways[i][j] = 
+						cnts[i][j] = max(cnts[i][j], cnts[now.first][now.second] + 1);
+						if (!vis[i][j])
+						{
+							vis[i][j] = true;
+							qu.emplace(pii{i, j});
+						}
 						break;
 					}
 				}
 		}
+
+		printf("%d\n", cnts[n][m] + fix);
 	}
 	return 0;
 }
