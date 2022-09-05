06b0f9715feddd997266016f971643b3fe01267d
OEmiliatanO <emiliaistruelove@gmail.com>
2022-09-05 18:28:17 +0800

    update 612

---
+++ 612/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+constexpr int maxn = 100;
+string arr[maxn + 1];
+
+int main()
+{
+	int M;
+	cin >> M;
+	while(M--)
+	{
+		int n, m;
+		cin >> n >> m; cin.ignore();
+		for (int i = 0; i < m; ++i) getline(cin, arr[i]);
+		sort(arr, arr + m, [](const string& lhs, const string& rhs) {
+				int l = 0, r = 0;
+				for (auto it = lhs.begin(); it != lhs.end(); ++it)
+					for (auto comp = it + 1; comp != lhs.end(); ++comp)
+						l += (*it > *comp);
+				for (auto it = rhs.begin(); it != rhs.end(); ++it)
+					for (auto comp = it + 1; comp != rhs.end(); ++comp)
+						r += (*it > *comp);
+				return l < r;
+				});
+		for (int i = 0; i < m; ++i) cout << arr[i] << '\n';
+	}
+
+	return 0;
+}
---
+++ template/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
