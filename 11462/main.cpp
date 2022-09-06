fc6dabd8c66cbc781fa0c15f6752b7760693c9c5
OEmiliatanO <emiliaistruelove@gmail.com>
2022-09-06 13:51:32 +0800

    update

--- 11462/main.cpp
+++ 11462/main.cpp
@@ -11,22 +11,25 @@ int ages[maxn + 1];
 int main()
 {
 	ios_base::sync_with_stdio(false);
-	//cin.tie(0);
+	cin.tie(0);
 
 	int n;
 	while(cin >> n, n)
 	{
+		int maxage = 0;
 		for (int i = 0, x; i < n; ++i)
 		{
 			cin >> x;
+			maxage = max(maxage, x);
 			++ages[x];
 		}
-
-		for (int i = 1; i <= 100; ++i)
+		
+		for (int i = 1; i <= maxage; ++i)
 			while(ages[i])
 			{
-				cout << i << " ";
-				--ages[i];
+				cout << i;
+				if (ages[i] != 1 || i != maxage) cout << ' ';
+				ages[i]--;
 			}
 		cout << '\n';
 	}
